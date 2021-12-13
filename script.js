05c4cfb08b3148eb5fcbf7de8331024fb2bf3e8a
Aldiyar <aldiyar-bolegenov@mail.ru>
2021-12-13 15:01:47 +0600

    js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+"use strict";
+if(4 == 9) {
+    console.log("ok");
+}
+else {
+    console.log("no");
+}
 
+const num = 50;
+
+if(num<49) {
+    console.log("Error");
+}
+else if (num > 100) {
+    console.log("too much");
+}
+else {
+    console.log("okay");
+}
+
+let num = 40;
+(num == 50 ) ? console.log("okay") : console.log("Error");
+
+const num = 500;
+
+switch (num) {
+    case 49: 
+        console.log("error");
+        break;
+    case 100:
+        console.log("error");
+        break;
+    case 50:
+        console.log("clear");
+        break;
+    default:
+        console.log("nnn");
+        break;
+}
